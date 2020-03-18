2f646143309f57e0d2598e4b52e8fb3b397676cd
adam <athornton@gmail.com>
2020-03-18 12:37:19 -0700

    Don't remove old classes just yet.

--- jupyterlabutils/notebook/__init__.py
+++ jupyterlabutils/notebook/__init__.py
@@ -3,9 +3,11 @@ Collection of notebook utilities
 """
 from .catalog import get_catalog, retrieve_query
 from .forwarder import Forwarder
+from .clusterproxy import ClusterProxy
+from .lsstdaskclient import LSSTDaskClient
 from .utils import format_bytes, get_proxy_url, get_hostname, \
     show_with_bokeh_server
 
-__all__ = [Forwarder, format_bytes,
+__all__ = [ClusterProxy, LSSTDaskClient, Forwarder, format_bytes,
            get_catalog, retrieve_query, get_proxy_url, get_hostname,
            show_with_bokeh_server]
